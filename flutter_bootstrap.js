53518f416ad42849a13c9afc965fa793c441fed5
mohamed6572 <93680801+mohamed6572@users.noreply.github.com>
2026-06-25 20:28:28 +0300

    Force update portfolio with latest Flutter web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3105299567" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3316746531" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
